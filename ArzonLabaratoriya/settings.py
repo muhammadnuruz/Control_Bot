d9a2c007329953074c6e4195ef79ff90c3ea1a85
muhammadnur <muhammadnurpdp@gmail.com>
2024-11-12 17:19:49 +0500

    first commit

--- ArzonLabaratoriya/settings.py
+++ ArzonLabaratoriya/settings.py
@@ -65,7 +65,7 @@ ROOT_URLCONF = 'ArzonLabaratoriya.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'apps/templates')],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
