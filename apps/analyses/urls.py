43d6cddd78b3827700634e747c6fe901ffbbc7cf
muhammadnur <muhammadnurpdp@gmail.com>
2024-12-02 11:48:19 +0500

    first commit

--- apps/analyses/urls.py
+++ apps/analyses/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('messages/create/', MessageCreateView.as_view(), name='message-create'),
     path('messages/<str:chat_id>/', MessageRetrieveView.as_view(), name='message-retrieve'),
     path('messages/statistics/', StatisticsAPIView.as_view(), name='statistics'),
-    path('messages/<int:pk>/delete/', MessageDeleteView.as_view(), name='message-delete'),
+    path('messages/<chat_id:str>/delete/', MessageDeleteView.as_view(), name='message-delete'),
 ]
