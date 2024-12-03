fb3f3f8aa3c34d85863f6e8dd8b3355c390b5ddf
muhammadnur <muhammadnurpdp@gmail.com>
2024-12-03 22:07:28 +0500

    first commit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import asyncio
-from aiogram import Bot, Dispatcher, types
+from aiogram import Bot, Dispatcher
+from aiogram.types import Message
 from aiogram.dispatcher.filters import Command
 from aiogram.utils import executor
 import requests
