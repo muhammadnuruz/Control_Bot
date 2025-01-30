5d55456ec991cfecc8fbfa1d2974b2c3c6265493
muhammadnur <muhammadnurpdp@gmail.com>
2025-01-30 23:10:17 +0500

    first commit

--- apps/groups/admin.py
+++ apps/groups/admin.py
@@ -4,13 +4,13 @@ from apps.groups.models import Groups
 
 @admin.register(Groups)
 class GroupsAdmin(admin.ModelAdmin):
-    list_display = ('chat_id', 'get_owners', 'created_at')  # get_owners funksiyasidan foydalanamiz
+    list_display = ('chat_id', 'get_companies', 'created_at')  # get_companies funksiyasidan foydalanamiz
     list_filter = ('created_at',)  # Filtrlash uchun maydon
     search_fields = ('chat_id',)  # Qidiruv maydoni
     ordering = ('-created_at',)  # Oxirgi qo‘shilganlarni birinchi ko‘rsatish
     date_hierarchy = 'created_at'  # Vaqt bo‘yicha tez navigatsiya qilish
 
-    def get_owners(self, obj):
-        return ", ".join([owner.name for owner in obj.owners.all()])
+    def get_companies(self, obj):
+        return ", ".join([owner.name for owner in obj.companies.all()])
 
-    get_owners.short_description = 'Owners'  # Admin paneldagi ustun nomi
+    get_companies.short_description = 'Companies'  # Admin paneldagi ustun nomi
